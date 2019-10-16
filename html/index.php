3e9d23b0b2fb709443d0585e0e33e88801503c4a
James Douglas <james@douglastechnology.co.uk>
2019-10-16 14:30:47 +0100

    initial layout

--- html/index.php
+++ html/index.php
@@ -25,6 +25,8 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
 <head>
   <style>
     *:not(hr) { box-sizing: inherit; }
+    html, body { height: 100vh; overflow: hidden; margin: 0; padding: 0; }
+    .question { margin-bottom: 0.5em; padding: 0.5em; border: 1px solid black; }
   </style>
   <script src="jquery.js"></script>
   <script src="markdown-it.js"></script>
@@ -39,12 +41,31 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
     });
   </script>
 </head>
-<body>
-  <?if(!$uuid){?><input id="register" type="button" value="register"><?}?>
-  <textarea></textarea>
-  <input id="send" type="button" value="send">
-  <?foreach(db("select chat_markdown from chat order by chat_at desc") as $r){ extract($r);?>
-    <div class="markdown" data-markdown="<?=htmlspecialchars($chat_markdown)?>"></div>
-  <?}?>
+<body style="display: flex; background-color: red;">
+  <main style="background-color: lightgreen; display: flex; flex-direction: column; flex: 0 0 70%;">
+    <header style="background-color: brown; padding: 0.5em; border-bottom: 2px solid black;">
+      <span>TopAnswers: Meta</span>
+    </header>
+    <div id="qa" style="background-color: goldenrod; overflow-y: scroll; padding: 0.5em;">
+      <?for($x = 0; $x<100; $x++){?>
+        <div class="question"><?=$x?>) Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tristique placerat felis vitae cursus.</div>
+      <?}?>
+    </div>
+  </main>
+  <div id="chat" style="background-color: darkslateblue; flex: 0 0 30%; display: flex; flex-direction: column; justify-content: flex-end; border-left: 2px solid black;">
+    <header style="background-color: seagreen; padding: 0.5em; border-bottom: 2px solid black;">
+      <span>The Tavern</span>
+    </header>
+    <div style="display: flex; flex-direction: column-reverse; overflow-y: scroll; padding: 0.5em;">
+      <?foreach(db("select chat_markdown from chat order by chat_at desc") as $r){ extract($r);?>
+        <div class="markdown" data-markdown="<?=htmlspecialchars($chat_markdown)?>"></div>
+      <?}?>
+    </div>
+    <div style="padding: 0.5em;">
+      <?if(!$uuid){?><input id="register" type="button" value="register"><?}?>
+      <textarea></textarea>
+      <input id="send" type="button" value="send">
+    </div>
+  </div>
 </body>   
 </html>   
