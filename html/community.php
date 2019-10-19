b46581c6d074586f0aa2a02ea3ac50f5332d65f3
James Douglas <james@douglastechnology.co.uk>
2019-10-19 11:03:28 +0100

    name above chat messages

--- html/community.php
+++ html/community.php
@@ -33,16 +33,17 @@ $room = $_GET['room'] ?? ccdb("select community_room_id from community where com
     @font-face { font-family: 'Quattrocento'; src: url('/Quattrocento-Regular.ttf') format('truetype'); font-weight: normal; font-style: normal; }
     @font-face { font-family: 'Quattrocento'; src: url('/Quattrocento-Bold.ttf') format('truetype'); font-weight: bold; font-style: normal; }
     html, body { height: 100vh; overflow: hidden; margin: 0; padding: 0; }
-    header { font-size: 1rem; background-color: #4d7ebb; padding: 0.5em; }
+    header { font-size: 1rem; background-color: #4d7ebb; }
     .question { margin-bottom: 0.5em; padding: 0.5em; border: 1px solid darkgrey; }
     .message { flex: 0 0 auto; max-width: calc(100% - 1.7em); max-height: 8em; overflow: auto; padding: 0.2em; border: 1px solid darkgrey; border-radius: 0.3em; background-color: white; }
-    .message-wrapper { width: 100%; display: flex; margin-top: 0.2em; align-items: center; }
-    .message-wrapper>img { flex: 0 0 1.2em; height: 1.2em; margin-right: 0.2em; xborder: 1px solid black; xborder-radius: 2px; }
+    .message-wrapper { width: 100%; margin-top: 0.2em; position: relative; display: flex; }
+    .message-wrapper>small { font-size: 0.6em; position: absolute; top: -1.2em; }
+    .message-wrapper>img { flex: 0 0 1.2em; height: 1.2em; margin-right: 0.2em; margin-top: 0.1em; }
     .markdown>:first-child { margin-top: 0; }
     .markdown>:last-child { margin-bottom: 0; }
     .markdown ul { padding-left: 1em; }
     .markdown img { max-height: 7em; }
-    .activeUser { height: 1.5em; width: 1.5em; margin: 0.1em; }
+    .active-user { height: 1.5em; width: 1.5em; margin: 0.1em; }
   </style>
   <script src="/jquery.js"></script>
   <script src="/markdown-it.js"></script>
@@ -107,14 +108,19 @@ $room = $_GET['room'] ?? ccdb("select community_room_id from community where com
 </head>
 <body style="display: flex; background-color: red;">
   <main style="background-color: lightgreen; display: flex; flex-direction: column; flex: 0 0 60%;">
-    <header style="border-bottom: 2px solid black;">
-      <span>TopAnswers: </span>
-      <select id="community">
-        <?foreach(db("select community_name from community order by community_name desc") as $r){ extract($r);?>
-          <option<?=($community===$community_name)?' selected':''?>><?=ucfirst($community_name)?></option>
-        <?}?>
-      </select>
-      <?if(!$uuid){?><input id="register" type="button" value="register"><?}?>
+    <header style="border-bottom: 2px solid black; display: flex; align-items: center; justify-content: space-between;">
+      <div style="margin: 0.5em;">
+        <span>TopAnswers: </span>
+        <select id="community">
+          <?foreach(db("select community_name from community order by community_name desc") as $r){ extract($r);?>
+            <option<?=($community===$community_name)?' selected':''?>><?=ucfirst($community_name)?></option>
+          <?}?>
+        </select>
+      </div>
+      <div style="height: 100%">
+        <?if(!$uuid){?><input id="register" type="button" value="register"><?}?>
+        <a href="/profile"><img style="background-color: #d4dfec; padding: 0.2em; display: block;" src="/identicon.php?id=<?=crc32(ccdb("select account_id from login where login_uuid=$1",$uuid))?>"></a>
+      </div>
     </header>
     <div id="qa" style="background-color: white; overflow: auto; padding: 0.5em;">
       <?for($x = 1; $x<100; $x++){?>
@@ -123,7 +129,7 @@ $room = $_GET['room'] ?? ccdb("select community_room_id from community where com
     </div>
   </main>
   <div id="chat" style="background-color: #d4dfec; flex: 0 0 40%; display: flex; flex-direction: column-reverse; justify-content: flex-start; min-width: 0; xoverflow-x: auto; border-left: 2px solid black;">
-    <header style="flex: 0 0 auto; border-top: 2px solid black;">
+    <header style="flex: 0 0 auto; border-top: 2px solid black; padding: 0.5em;">
       <select id="room">
         <?foreach(db("select room_id, coalesce(room_name,initcap(community_name)||' Chat') room_name from room where community_name=$1 order by room_name desc",$community) as $r){ extract($r);?>
           <option<?=($room_id===$room)?' selected':''?> value="<?=$room_id?>"><?=$room_name?></option>
@@ -136,16 +142,18 @@ $room = $_GET['room'] ?? ccdb("select community_room_id from community where com
     <?}?>
     <div style="display: flex; flex: 1 1 auto; min-height: 0;">
       <div style="flex: 1 1 auto; display: flex; align-items: flex-start; flex-direction: column-reverse; padding: 0.5em; overflow: scroll;">
-        <?foreach(db("select account_id, chat_markdown from chat where room_id=$1 order by chat_at desc",$room) as $r){ extract($r);?>
+        <?foreach(db("select account_id,coalesce(nullif(account_name,''),'Anonymous') account_name,chat_markdown from chat natural join login where room_id=$1 order by chat_at desc",$room) as $r){ extract($r);?>
           <div class="message-wrapper">
+            <small><?=$account_name?>:</small>
             <img src="/identicon.php?id=<?=crc32($account_id)?>">
             <div class="message markdown" data-markdown="<?=htmlspecialchars($chat_markdown)?>"></div>
           </div>
+          <div class="spacer" style="height: 1em;"></div>
         <?}?>
       </div>
-      <div id="activeUsers" style="display: flex; flex-direction: column-reverse; align-items: flex-start; background-color: #e7edf4; border-left: 1px solid darkgrey; padding: 0.1em; overflow-y: hidden;">
-        <?foreach(db("select account_id from chat group by account_id having max(chat_at)>(current_timestamp-'1d'::interval) order by max(chat_at)") as $r){ extract($r);?>
-          <img class="activeUser" src="/identicon.php?id=<?=crc32($account_id)?>">
+      <div id="active-users" style="display: flex; flex-direction: column-reverse; align-items: flex-start; background-color: #e7edf4; border-left: 1px solid darkgrey; padding: 0.1em; overflow-y: hidden;">
+        <?foreach(db("select account_id from chat where room_id=$1 group by account_id having max(chat_at)>(current_timestamp-'1d'::interval) order by max(chat_at) desc",$room) as $r){ extract($r);?>
+          <img class="active-user" src="/identicon.php?id=<?=crc32($account_id)?>">
         <?}?>
       </div>
     </div>
