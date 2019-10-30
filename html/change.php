bc405bdd88d0daa98a87dd1ff97951152fb06ca8
Jack Douglas <jack@douglastechnology.co.uk>
2019-10-30 14:53:24 +0000

    update chat when notification received from another room

--- html/change.php
+++ html/change.php
@@ -17,5 +17,5 @@ header("Cache-Control: post-check=0, pre-check=0", false);
 $uuid = $_COOKIE['uuid'] ?? false;
 if($uuid) ccdb("select set_config('custom.uuid',$1,false)",$uuid);
 $room = $_GET['room'] or die('Missing room_id');
-echo ccdb("select (select json_build_object('question_change_id',1,'chat_change_id',coalesce(room_latest_chat_id,0)) from room where room_id=$1)",$room);
+echo ccdb("select (select json_build_object('question_change_id',1,'chat_change_id',coalesce(greatest(chat_change_id,(select max(chat_change_id) from chat_notification natural join chat)),0)) from room natural join chat where chat_id=room_latest_chat_id and room_id=$1)",$room);
 ?>
