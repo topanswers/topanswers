59483b0be164f345353bb65faf23f827a7342a90
James Douglas <james@douglastechnology.co.uk>
2020-01-29 11:04:02 +0000

    fix notification regression and cleanup api

--- get/notification.php
+++ get/notification.php
@@ -23,7 +23,7 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , to_char(chat_at,'YYYY-MM-DD".'"T"'."HH24:MI:SS".'"Z"'."') notification_at_iso
                                     , encode(community_mid_shade,'hex') notification_mid_shade
                                     , encode(community_dark_shade,'hex') notification_dark_shade
-                                    , encode(community_warning_shade,'hex') notification_warning_shade
+                                    , encode(community_warning_color,'hex') notification_warning_shade
                                     , community_name notification_community_name
                                     , question_id
                                     , null::text question_title
@@ -53,7 +53,7 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , to_char(question_notification_at,'YYYY-MM-DD".'"T"'."HH24:MI:SS".'"Z"'."') notification_at_iso
                                     , encode(community_mid_shade,'hex') notification_mid_shade
                                     , encode(community_dark_shade,'hex') notification_dark_shade
-                                    , encode(community_warning_shade,'hex') notification_warning_shade
+                                    , encode(community_warning_color,'hex') notification_warning_shade
                                     , community_name notification_community_name
                                     , question_id
                                     , question_title
@@ -69,7 +69,7 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , to_char(question_flag_notification_at,'YYYY-MM-DD".'"T"'."HH24:MI:SS".'"Z"'."') notification_at_iso
                                     , encode(community_mid_shade,'hex') notification_mid_shade
                                     , encode(community_dark_shade,'hex') notification_dark_shade
-                                    , encode(community_warning_shade,'hex') notification_warning_shade
+                                    , encode(community_warning_color,'hex') notification_warning_shade
                                     , community_name notification_community_name
                                     , question_id
                                     , question_title
@@ -77,12 +77,12 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , null::boolean
                                     , null::integer
                                     , null::integer, null::integer, null::text, null::integer, null::integer, null::boolean, null::boolean, null::text, null::text, null::text, null::boolean, null::boolean, null::boolean
-                               from (select question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_shade
+                               from (select question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_color
                                           , max(question_flag_history_id) question_flag_history_id
                                           , max(question_flag_notification_at) question_flag_notification_at
                                           , count(distinct account_id) question_flag_count
                                      from question_flag_notification
-                                     group by question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_shade) n )
+                                     group by question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_color) n )
                        , a as (select 'answer' notification_type
                                     , 1 notification_count
                                     , answer_history_id notification_id
@@ -90,7 +90,7 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , to_char(answer_notification_at,'YYYY-MM-DD".'"T"'."HH24:MI:SS".'"Z"'."') notification_at_iso
                                     , encode(community_mid_shade,'hex') notification_mid_shade
                                     , encode(community_dark_shade,'hex') notification_dark_shade
-                                    , encode(community_warning_shade,'hex') notification_warning_shade
+                                    , encode(community_warning_color,'hex') notification_warning_shade
                                     , community_name notification_community_name
                                     , question_id
                                     , question_title
@@ -106,7 +106,7 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , to_char(answer_flag_notification_at,'YYYY-MM-DD".'"T"'."HH24:MI:SS".'"Z"'."') notification_at_iso
                                     , encode(community_mid_shade,'hex') notification_mid_shade
                                     , encode(community_dark_shade,'hex') notification_dark_shade
-                                    , encode(community_warning_shade,'hex') notification_warning_shade
+                                    , encode(community_warning_color,'hex') notification_warning_shade
                                     , community_name notification_community_name
                                     , question_id
                                     , question_title
@@ -114,12 +114,12 @@ $notification_count = ccdb("select count(1) from chat_notification")
                                     , null::boolean
                                     , null::integer
                                     , null::integer, null::integer, null::text, null::integer, null::integer, null::boolean, null::boolean, null::text, null::text, null::text, null::boolean, null::boolean, null::boolean
-                               from (select answer_id,question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_shade
+                               from (select answer_id,question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_color
                                           , max(answer_flag_history_id) answer_flag_history_id
                                           , max(answer_flag_notification_at) answer_flag_notification_at
                                           , count(distinct account_id) answer_flag_count
                                      from answer_flag_notification
-                                     group by answer_id,question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_shade) n)
+                                     group by answer_id,question_id,question_title,community_name,community_mid_shade,community_dark_shade,community_warning_color) n)
                     select * from c union all select * from q union all select * from qf union all select * from a union all select * from af
                     order by notification_at limit 20") as $r){ extract($r);?>
         <div id="n<?=$notification_id?>" class="notification<?=($notification_type==='chat')?' message':''?>" style="background: #<?=$notification_mid_shade?>; --colour-dark: #<?=$notification_dark_shade?>; --colour-warning: #<?=$notification_warning_shade?>; --colour-dark-99: #<?=$notification_dark_shade?>99;" data-id="<?=$notification_id?>" data-type="<?=$notification_type?>"<?if($notification_type==='chat'){?> data-name="<?=$chat_from_account_name?>" data-reply-id="<?=$chat_reply_id?>"<?}?>>
