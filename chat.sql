8e10cc99c955a9fdeecc59e3e7bd6cfa4d7b2b6f
Jack Douglas <jack@douglastechnology.co.uk>
2020-02-17 20:03:02 +0000

    api cleanup part 2

--- chat.sql
+++ chat.sql
@@ -67,54 +67,6 @@ create function range(startid bigint, endid bigint)
   where chat_id>startid or endid is not null
   order by chat_at desc;
 $$;
-create function range2(startid bigint, endid bigint)
-                     returns table (chat_id bigint
-                                  , account_id integer
-                                  , chat_reply_id integer
-                                  , chat_markdown text
-                                  , chat_at timestamptz
-                                  , chat_change_id bigint
-                                  , account_is_me boolean
-                                  , account_name text
-                                  , reply_account_name text
-                                  , reply_account_is_me boolean
-                                  , chat_gap integer
-                                  , communicant_votes integer
-                                  , chat_editable_age boolean
-                                  , i_flagged boolean
-                                  , i_starred boolean
-                                  , chat_account_will_repeat boolean
-                                  , chat_flag_count integer
-                                  , chat_star_count integer
-                                  , chat_has_history boolean
-                                  , chat_pings json
-                                  , chat_account_is_repeat boolean
-                                  , rn bigint
-                                   ) language sql security definer set search_path=db,api,chat,pg_temp as $$
-  with g as (select get_account_id() account_id)
-  select *, row_number() over(order by chat_at desc) rn
-  from (select *, (lead(account_id) over (order by chat_at desc)) is not distinct from account_id and chat_reply_id is null and chat_gap<60 chat_account_is_repeat
-        from (select chat_id,account_id,chat_reply_id,chat_markdown,chat_at,chat_change_id
-                   , account_id=(select account_id from g) account_is_me
-                   , coalesce(nullif(account_name,''),'Anonymous') account_name
-                   , (select coalesce(nullif(account_name,''),'Anonymous') from chat natural join account where chat_id=c.chat_reply_id) reply_account_name
-                   , (select account_id=(select account_id from g) from chat natural join account where chat_id=c.chat_reply_id) reply_account_is_me
-                   , round(extract('epoch' from chat_at-(lead(chat_at) over (order by chat_at desc))))::integer chat_gap
-                   , coalesce(communicant_votes,0) communicant_votes
-                   , extract('epoch' from current_timestamp-chat_at)<240 chat_editable_age
-                   , exists(select 1 from chat_flag where chat_id=c.chat_id and account_id=(select account_id from g)) i_flagged
-                   , exists(select 1 from chat_star where chat_id=c.chat_id and account_id=(select account_id from g)) i_starred
-                   , (lead(account_id) over (order by chat_at desc)) is not distinct from account_id and chat_reply_id is null and (lead(chat_reply_id) over (order by chat_at desc)) is null chat_account_will_repeat
-                   , (select count(1)::integer from chat_flag where chat_id=c.chat_id) chat_flag_count
-                   , (select count(1)::integer from chat_star where chat_id=c.chat_id) chat_star_count
-                   , (select count(1) from chat_history where chat_id=c.chat_id)>1 chat_has_history
-                   , (select json_agg(p.account_id) from ping p where p.chat_id=c.chat_id and c.account_id=get_account_id()) chat_pings
-              from chat c natural join account natural join (select account_id,community_id,communicant_votes from communicant) v
-              where room_id=get_room_id() and chat_id>=startid and (endid is null or chat_id<=endid)) z
-        where (select account_id from g) is not null or chat_flag_count=0) z
-  where chat_id>startid or endid is not null
-  order by chat_at desc;
-$$;
 --
 create function activerooms() returns table (room_id integer, question_id integer, room_name text, community_name text, room_account_unread_messages bigint, room_account_latest_read_chat_id bigint)
                 language sql security definer set search_path=db,api,chat,pg_temp as $$
