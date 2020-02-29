8ec6a86cbc5c558d0357ebc25b3164ce9504f0fd
Pax <pax.margret@gmail.com>
2020-02-29 09:18:02 -0800

    :card_file_box::whale: Seed the database with initial data

---
+++ docker/postgres/seeds/codelicense.sql
@@ -0,0 +1,12 @@
+--
+-- Data for Name: codelicense; Type: TABLE DATA; Schema: out; Owner: postgres
+--
+
+COPY "db".codelicense (codelicense_id, codelicense_name, codelicense_is_versioned, codelicense_description) FROM stdin;
+2	CC0 1.0	t	CC0 1.0 Universal Public Domain Dedication
+1	no dual code license	f	
+3	LPPL Version 1.3c	t	The LaTeX project public license (LPPL), version 1.3c
+4	MIT	f	The MIT License
+5	GPLv2	t	GNU General Public License, version 2
+6	GPLv3	t	GNU General Public License
+\.
