13058c795f4349d659e649c005b101b32b3adfea
Pax <pax.margret@gmail.com>
2020-03-03 15:16:35 -0800

    :card_file_box: Seed `source` table

---
+++ docker/postgres/seeds/source.sql
@@ -0,0 +1,10 @@
+--
+-- Data for Name: source; Type: TABLE DATA; Schema: db; Owner: postgres
+--
+
+COPY "db".source (community_id, sesite_id, source_is_default) FROM stdin;
+2	1	t
+4	2	t
+7	4	t
+2	6	f
+\.
