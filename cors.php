b14820e58f98f14874932acd717d69383ee78ff8
Pax <pax.margret@gmail.com>
2020-03-23 19:57:37 -0700

    :wrench: Do not hardcode origin URL

--- cors.php
+++ cors.php
@@ -1,5 +1,5 @@
 <?    
 header('X-Powered-By: ');
-header('Access-Control-Allow-Origin: https://topanswers.xyz');
+header('Access-Control-Allow-Origin: https://' . config("SITE_DOMAIN"));
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-methods: POST');
