53911216eab6023c7bff93eedcad8ffa858571a1
Pax <pax.margret@gmail.com>
2020-03-25 18:35:21 -0700

    :white_check_mark::art: Remove test logic from spec file

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -1,37 +1,24 @@
+import { StubConfirmAndAlertBox, AssertConfirmationKeywords, 
+    RetrieveAccountToken, LoginAndAssertUserIconVisibility 
+} from "./registration.scripts"
+
 describe("The registration and login process", () => {
-  specify("should give a token", () => {
-    const windowConfirmStub = cy.stub();
-    cy.on("window:confirm", windowConfirmStub);
-    const windowAlertStub = cy.stub();
-    cy.on("window:alert", windowAlertStub);
+  specify("Registration gives a token which is used for logging in", () => {
+      
+    StubConfirmAndAlertBox()
 
     cy.visit("/");
     cy.get("#join")
       .click()
       .then(() => {
-        cy.wrap(windowConfirmStub.firstCall)
-          .its("args.0").then((confirmText) => {
-              expect(confirmText).to.have.string("set a cookie")
-              expect(confirmText).to.have.string("You must be 16 or over")
-          })
-
-        cy.wrap(windowAlertStub.firstCall).then(alert => {
-          const match = alert.args[0].match(/\n\n([-a-zA-Z0-9]+)/);
-          cy.wrap(match[1]).as("loginKey");
-        });
+        AssertConfirmationKeywords()
+        RetrieveAccountToken()
       });
 
     cy.get("[test-id=userIcon]").should("be.visible");
-
-    cy.clearCookies();
     
+    cy.clearCookies();
     cy.visit("/");
-    cy.window().then(win => {
-      cy.get("@loginKey").then(loginKey => {
-        cy.stub(win, "prompt").returns(loginKey);
-        cy.get("[test-id=loginBtn]").click();
-        cy.get("[test-id=userIcon]").should("be.visible");
-      });
-    });
+    LoginAndAssertUserIconVisibility()
   });
 });
