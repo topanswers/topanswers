819b3877de56a5bbf3af2b38464b1620acf871f1
Jack Douglas <jack@douglastechnology.co.uk>
2020-04-13 12:54:57 +0100

    fix cosmetic dbfiddle php warnings/notices

--- get/dbfiddle.php
+++ get/dbfiddle.php
@@ -18,31 +18,34 @@ header('Cache-Control: max-age=60');
 ?>
 <div class="dbfiddle" data-rdbms="<?=$_GET['rdbms']?>">
   <?foreach($json as $i=>$batchplusoutput){?>
+    <?if(!array_key_exists($i,$hide)) $hide[$i] = false;?>
     <div class="batch<?=$hide[$i]?' hidden':''?>">
       <textarea rows="1"><?=$batchplusoutput['batch']?></textarea>
       <?if($batchplusoutput['output']['error']){?>
         <pre class="error"><?=$batchplusoutput['output']['error']?></pre>
       <?}else{?>
         <?foreach($batchplusoutput['output']['result'] as $result){?>
-          <div class="tablewrapper">
-            <table>
-              <tbody>
-                <tr>
-                  <?foreach($result['head'] as $head){?>
-                    <th><?=$head?></th>
-                  <?}?>
-                </tr>
-                <?foreach(transpose($result['data']) as $row){?>
+          <?if($result['head']){?>
+            <div class="tablewrapper">
+              <table>
+                <tbody>
                   <tr>
-                    <?foreach($row as $data){?>
-                      <td><?=htmlspecialchars($data)?></td>
+                    <?foreach($result['head'] as $head){?>
+                      <th><?=$head?></th>
                     <?}?>
-                  <tr>
-                <?}?>
-              </tbody>
-            </table>
-          </div>
-          <?if($head==='Microsoft SQL Server 2005 XML Showplan'){?><div class="qp" data-xml="<?=htmlspecialchars($data)?>"></div><?}?>
+                  </tr>
+                  <?foreach(transpose($result['data']) as $row){?>
+                    <tr>
+                      <?foreach($row as $data){?>
+                        <td><?=htmlspecialchars($data)?></td>
+                      <?}?>
+                    <tr>
+                  <?}?>
+                </tbody>
+              </table>
+            </div>
+          <?}?>
+          <?if(isset($head)) if($head==='Microsoft SQL Server 2005 XML Showplan'){?><div class="qp" data-xml="<?=htmlspecialchars($data)?>"></div><?}?>
         <?}?>
       <?}?>
     </div>
