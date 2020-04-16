540d3e484817eb067f645a7d6405beff7482a35b
Jack Douglas <jack@douglastechnology.co.uk>
2020-04-16 21:37:59 +0100

    fix room order regression

--- get/activerooms.php
+++ get/activerooms.php
@@ -8,7 +8,7 @@ db("set search_path to activerooms,pg_temp");
 ccdb("select login_community(nullif($1,'')::uuid,$2)",$_COOKIE['uuid']??'',$_GET['community']) || fail(403,'access denied');
 extract(cdb("select community_name,community_language
                   , (select jsonb_agg(z)
-                     from (select room_group, jsonb_agg(z) rooms
+                     from (select room_group, jsonb_agg(z order by rn) rooms
                            from (select room_id,room_derived_name,room_question_id,community_name,listener_latest_read_chat_id,listener_unread
                                       , case when room_question_id is not null and community_name=$1 then 1 when room_question_id is null then 2 else 3 end room_group
                                       , row_number() over (order by participant_chat_count desc, participant_latest_chat_at desc) rn
