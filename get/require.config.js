2da0b9480fae1e0735c662598c8a6c0e431f4a2c
Jack Douglas <jack@douglastechnology.co.uk>
2020-05-16 15:13:22 +0100

    community home page refactoring pt 1c: lets try again

--- get/require.config.js
+++ get/require.config.js
@@ -125,6 +125,6 @@ var require = {
     'tio':'../tio.0984e7f3a524834f',
     'navigation':'../navigation.c546209755df730d',
     'markdown':'../markdown.86706dd2b49a3dca',
-    'require.config':'../require.config.c6c4cda5434d144b',
+    'require.config':'../require.config.ce49c5591806713c',
  } },
 };
