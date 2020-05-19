4a6e58ed5dc666461c5c4c395f50d2c150fd57ed
Jack Douglas <jack@douglastechnology.co.uk>
2020-05-19 16:20:29 +0100

    add subtle box shadow to blockquotes

--- get/require.config.js
+++ get/require.config.js
@@ -132,7 +132,7 @@ var require = {
     'katex':'katex.9460c91fb69cca2c',
     'tio':'../tio.0984e7f3a524834f',
     'navigation':'../navigation.c546209755df730d',
-    'require.config':'../require.config.4ed8233f40eb323c',
+    'require.config':'../require.config.67168e0b05a3618d',
     'markdown':'../markdown.0f839de4a4f34578',
  } },
 };
