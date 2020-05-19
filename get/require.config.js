8e1d6caa8fbb9d9df52827c2ab9da06a015ca880
Jack Douglas <jack@douglastechnology.co.uk>
2020-05-19 10:51:29 +0100

    rejig blockquote (and code within blockquote) backgrounds

--- get/require.config.js
+++ get/require.config.js
@@ -132,7 +132,7 @@ var require = {
     'katex':'katex.9460c91fb69cca2c',
     'tio':'../tio.0984e7f3a524834f',
     'navigation':'../navigation.c546209755df730d',
-    'require.config':'../require.config.a0b588aba95123b1',
+    'require.config':'../require.config.31a5264eb7586012',
     'markdown':'../markdown.0f839de4a4f34578',
  } },
 };
