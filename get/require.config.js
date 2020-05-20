63b12cdb11d8fdbb6fe891f1f17e7b0ee169d762
Jack Douglas <jack@douglastechnology.co.uk>
2020-05-20 16:55:47 +0100

    fix another ios regression

--- get/require.config.js
+++ get/require.config.js
@@ -132,7 +132,7 @@ var require = {
     'katex':'katex.9460c91fb69cca2c',
     'tio':'../tio.0984e7f3a524834f',
     'navigation':'../navigation.c546209755df730d',
-    'require.config':'../require.config.84c1e0818df09e44',
+    'require.config':'../require.config.1150ca78cf1927f2',
     'markdown':'../markdown.0f839de4a4f34578',
  } },
 };
