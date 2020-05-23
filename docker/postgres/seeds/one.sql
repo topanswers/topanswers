e621d8095c5d7baf6ce4712150601fb53347150a
Pax <pax.margret@gmail.com>
2020-05-23 12:46:53 -0700

    :whale: Use Jack's script for creating and seeding db

--- docker/postgres/seeds/one.sql
+++
@@ -1,5 +0,0 @@
---
--- Data for Name: codelicense; Type: TABLE DATA; Schema: db; Owner: postgres
---
-
-INSERT INTO "db".one DEFAULT VALUES;
--- docker/postgres/seeds/source.sql
+++
@@ -1,9 +0,0 @@
---
--- Data for Name: source; Type: TABLE DATA; Schema: db; Owner: postgres
---
-
-COPY "db".source (community_id, sesite_id, source_is_default) FROM stdin;
-2	1	t
-4	2	t
-2	6	f
-\.
