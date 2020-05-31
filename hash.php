260170f942e1c10ede4953649f5d789e700fe7a9
Jack Douglas <jack@douglastechnology.co.uk>
2020-05-31 10:32:15 +0100

    switch soft keyboard to monospace

--- hash.php
+++ hash.php
@@ -329,7 +329,7 @@ function h($f){
     "/fonts/source-code-pro.css": "040b9fc96c140e1a",
     "/fonts/source-sans-pro.css": "7128abb0cd4de792",
     "/fonts/stix2.css": "5cf141611d69835c",
-    "/global.css": "49573030b953fa3e",
+    "/global.css": "ef899c7b9d24c259",
     "/header.css": "b7e87e6d5db23cbd",
     "/markdown.css": "f92ce042455f23a8",
     "/markdown.js": "0f80f10c4b9be8e3",
@@ -361,7 +361,7 @@ function h($f){
     "/page/user/user.css": "11c2caff7b24908b",
     "/page/user/user.js": "0bdeb71ac6f05fb9",
     "/post.css": "61e046b3a5fd7c26",
-    "/require.config.js": "18f6b6b33923c793",
+    "/require.config.js": "24d2e70a5cea847a",
     "/tio.js": "0984e7f3a524834f"
 }',true)[$f]).'.$2',$f);
 }
