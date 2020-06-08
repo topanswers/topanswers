a4da6ee61c078393ec2fde2ce65a736127eff84f
Jack Douglas <jack@douglastechnology.co.uk>
2020-06-08 14:22:48 +0100

    highlight direct linked fieldset in profile

--- get/require.config.js
+++ get/require.config.js
@@ -132,7 +132,7 @@ var require = {
     'vex':'vex.ce8070c89395b6ea',
     'markdown':'../markdown.167217915f5c0173',
     'navigation':'../navigation.f0ad7bc6582996ae',
-    'require.config':'../require.config.d16d61346621ab2a',
+    'require.config':'../require.config.7139edc6a0b3e613',
     'tio':'../tio.0984e7f3a524834f',
  } },
 };
