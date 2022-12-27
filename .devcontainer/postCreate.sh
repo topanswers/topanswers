8bfca8c34bc0ba751e6b4f9d624caf6e9d8bda40
Jack Douglas <floral.fosse-0a@icloud.com>
2022-12-27 14:18:46 +0000

    tweaked devcontainer for postgres server

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -3,7 +3,7 @@
 echo "deb http://apt.postgresql.org/pub/repos/apt bullseye-pgdg main" > /etc/apt/sources.list.d/pgdg.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
-export DEBIAN_FRONTEND=noninteractive && apt-get -y install --no-install-recommends postgresql-client-14 php xxhash
+export DEBIAN_FRONTEND=noninteractive && apt-get -y install --no-install-recommends postgresql-14 php xxhash
 
 cd
 wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
