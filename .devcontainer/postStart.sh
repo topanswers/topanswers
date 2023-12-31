ec05405cc2cbf1f3200e0460718e78fe03694ab7
Jack Douglas <jack@douglastechnology.co.uk>
2023-12-31 19:00:53 +0000

    update devcontainer

--- .devcontainer/postStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ip=$(wget -qO - icanhazip.com)
-
-#aws ec2 modify-security-group-rules \
-#    --group-id sg-0d230def00269dadb \
-#    --security-group-rules "SecurityGroupRuleId=sgr-0caf535bb0740e5d3,SecurityGroupRule={Description=Codespaces,IpProtocol=6,FromPort=5432,ToPort=5432,CidrIpv4=$ip/32}"
--- get/require.config.js
+++ get/require.config.js
@@ -144,7 +144,7 @@ var require = {
     'md-oneline':'../md-oneline.30cbe39e44603c66',
     'md-shortlinks':'../md-shortlinks.089468c314bf7e1c',
     'navigation':'../navigation.d0d12edff60010b2',
-    'require.config':'../require.config.292b025f6c10a542',
+    'require.config':'../require.config.a0ebbb0f6276e636',
     'tio':'../tio.84a530d72166be1e',
 
     }
